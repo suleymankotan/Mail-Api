bbf7ccee6a9b58839c506775492f67e0cf97abb3
suleymankotan <suleymankotan@gmail.com>
2020-09-14 21:59:31 +0300

    k8s deployment.yaml added

--- src/main/java/com/suleyman/mailsenderapi/controller/MailSendController.java
+++ src/main/java/com/suleyman/mailsenderapi/controller/MailSendController.java
@@ -1,11 +1,8 @@
 package com.suleyman.mailsenderapi.controller;
 
-import com.suleyman.mailsenderapi.entity.MailUser;
-import com.suleyman.mailsenderapi.repository.MailUserRepository;
-import com.suleyman.mailsenderapi.util.AES;
+
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
